03fbf7f9459c85bd3cc37b7ef4e6c9192c1044b2
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-28 15:13:08 +0700

    menambahkan fungsi main

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -31,6 +31,15 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    
+    return 0;
 }
 
