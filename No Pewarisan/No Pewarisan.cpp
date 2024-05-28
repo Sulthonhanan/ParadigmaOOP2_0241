5592e95495a31ce0c49e16abb9a6cd46fc7be342
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-28 15:30:51 +0700

    Menambahkan kelas baseClass dengan fungsi perkenalan

---
+++ No Pewarisan/No Pewarisan.cpp
@@ -0,0 +1,17 @@
+// No Pewarisan.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+public:
+    virtual void perkenalan() {
+        cout << " Halo saya Function dari base class";
+
+    }
+
+
+};
+
+
